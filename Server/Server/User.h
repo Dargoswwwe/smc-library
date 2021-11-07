520ff89e03db60b6039e9579eee6b8cc655f8f54
Dragos Ludu <lududragos@yahoo.com>
2021-11-07 11:31:10 +0200

    added getters, setters and a few more methods to User class

--- Server/Server/User.h
+++ Server/Server/User.h
@@ -11,11 +11,28 @@ private:
 	bool active;
 
 public:
-	User(const std::string& name, const std::string& pswrd);
+	User();
+	User(const std::string& name, const std::string& pswrd);		//recommended
+	User(const std::string& name, const std::string& pswrd, const std::vector<Book>& booksRead,
+		const std::vector<Book>& booksBorrowing, const std::vector<Book>& booksBorrowed,const bool& activ);
 
-	void Borrowed(const Book& book);
-	void Returned(const Book& book);
+	void Borrowed(const Book& book);		//recommended
+	void Returned(const Book& book);		//recommended
+	bool HasBook(const Book& book);
+	
 	void SetActivity(const bool& actv);
+	void SetUsername(const std::string& name);
+	void SetPassword(const std::string& pswrd);
+	void SetRead(const std::vector<Book>& booksRead);
+	void SetBorrowing(const std::vector<Book>& booksBorrowing);
+	void SetBorrowed(const std::vector<Book>& booksBorrowed);
 
-}
+	std::vector<Book> GetRead()const;
+	std::vector<Book> GetBorrowing()const;
+	std::vector<Book> GetBorrowed()const;
+	std::string GetUsername()const;
+	std::string GetPassword()const;
+	bool GetActivity()const;
+
+};
 
