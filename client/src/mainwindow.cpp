b46f880e1d9862f04291230c8122d9e218f480ad
Albert Geantă <albertgeanta@gmail.com>
2021-11-07 20:51:21 +0200

    Add titles to login/register and add button to switch between them

--- client/src/mainwindow.cpp
+++ client/src/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QObject::connect(ui->buttonRegisterInstead, &QPushButton::clicked, this, [this] { switchPage(1); });
+    QObject::connect(ui->buttonLoginInstead, &QPushButton::clicked, this, [this] { switchPage(0); });
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::switchPage(int pageIndex)
+{
+    ui->stackedWidget->setCurrentIndex(pageIndex);
+}
