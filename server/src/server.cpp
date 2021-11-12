327bf563423aa3170b3517ce4dcbd62f0608f317
Albert Geantă <albertgeanta@gmail.com>
2021-11-12 17:41:54 +0200

    Move server initialization to a separate function

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -3,11 +3,18 @@
 Server::Server(QObject* parent)
 {
     this->setParent(parent);
+
+    initServer();
+}
+
+void Server::initServer()
+{
     tcpServer = new QTcpServer(this);
     if (!tcpServer->listen(QHostAddress::LocalHost, 4200)) {
-        qWarning() << "Cannot start server!\n";
-        return;
-    }
+        qWarning() << "Cannot start server: " << tcpServer->serverError();
+        throw tcpServer->serverError();
+    } else qDebug() << "Started library server on port" << tcpServer->serverPort();
+}
 
     qDebug() << tcpServer->serverPort();
     tcpServer->setProperty("port", 4200);
